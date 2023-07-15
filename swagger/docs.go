b5517a73c0f536b3c229eef97b0be3ecad9d8b4b
code-horse-mi <945623397@qq.com>
2023-07-15 19:43:44 +0800

    完善Swagger文档功能

--- swagger/docs.go
+++ swagger/docs.go
@@ -6,10 +6,33 @@ import (
 )
 
 // Swagger文档
-func Docs(swo *spec.Swagger) {
+func DocsMcenter(swo *spec.Swagger) {
 	swo.Info = &spec.Info{
 		InfoProps: spec.InfoProps{
 			Title:       "注册中心",
+			Description: "Resource for managing Service Instances",
+			Contact: &spec.ContactInfo{
+				ContactInfoProps: spec.ContactInfoProps{
+					Name:  "john",
+					Email: "john@doe.rp",
+					URL:   "http://johndoe.org",
+				},
+			},
+			License: &spec.License{
+				LicenseProps: spec.LicenseProps{
+					Name: "MIT",
+					URL:  "http://mit.org",
+				},
+			},
+			Version: version.ShortVersion(),
+		},
+	}
+}
+
+func DocsMpaas(swo *spec.Swagger) {
+	swo.Info = &spec.Info{
+		InfoProps: spec.InfoProps{
+			Title:       "K8S运维管理中心",
 			Description: "Resource for managing Service Instances",
 			Contact: &spec.ContactInfo{
 				ContactInfoProps: spec.ContactInfoProps{
