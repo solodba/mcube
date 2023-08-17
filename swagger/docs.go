d99ecd9722dfec25a28f5ae4d5e16f0561cbf444
solodba <945623397@qq.com>
2023-08-17 22:11:27 +0800

    添加maudit swagger方法

--- swagger/docs.go
+++ swagger/docs.go
@@ -76,3 +76,27 @@ func DocsMpaas(swo *spec.Swagger) {
 		},
 	}
 }
+
+// Maudit Swagger文档
+func DocsMaudit(swo *spec.Swagger) {
+	swo.Info = &spec.Info{
+		InfoProps: spec.InfoProps{
+			Title:       "审计中心",
+			Description: "Resource for managing Service Instances",
+			Contact: &spec.ContactInfo{
+				ContactInfoProps: spec.ContactInfoProps{
+					Name:  "john",
+					Email: "john@doe.rp",
+					URL:   "http://johndoe.org",
+				},
+			},
+			License: &spec.License{
+				LicenseProps: spec.LicenseProps{
+					Name: "MIT",
+					URL:  "http://mit.org",
+				},
+			},
+			Version: version.ShortVersion(),
+		},
+	}
+}
